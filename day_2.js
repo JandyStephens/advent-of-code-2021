f723db709b941f48eac50b2183459ea61302984b
Jandy Stephens <36689860+JandyStephens@users.noreply.github.com>
2021-12-10 20:56:39 -0800

    clean up unnecessary comments

--- day_2.js
+++ day_2.js
@@ -1,14 +1,5 @@
 const fs = require("fs");
 
-// givenArr = [
-//   "forward 2",
-//   "forward 3",
-//   "forward 5",
-//   "forward 6",
-//   "down 7",
-//   "up 7",
-// ];
-
 const data = fs.readFileSync("day_2.txt", { encoding: "utf8", flag: "r" });
 console.log(data);
 
@@ -20,10 +11,7 @@ var givenArr = data.split("\n");
  */
 function plannedCourse(array) {
   forwardNum = 0;
-  // forwardIndex=[];
   depth = 0;
-  // OLD loop to find all 'forward', push all found indices to empty array, +1 to every 'forward' index, get sum of all +1 index values
-  // OLD loop through - if up/down, add/subtract value of next index to depth
 
   // loop through one line at a time: split on space (does that make it an array in an array?), if 0 index = 'forward', add 1 index to forward variable. if 0 index = "up"/"down", subtract/add value of 1 index to depth variable.
 
